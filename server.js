1d8dbb9d602c5c38512d21dc3e57813a8d51c098
Ubuntu <stefuhnee@leap-example-vm.bismxvmdxk3exkgxo5qgmw42ee.bx.internal.cloudapp.net>
2017-02-22 15:47:35 +0000

    made a file system route

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
-var http = require('http');
+'use strict';
 
-var server = http.createServer(function(request, response) {
-  response.write('Hello World!');
-  response.end();
+const http = require('http');
+const bluebird = require('bluebird');
+const fs = bluebird.promisifyAll(require('fs'));
+
+const express = require('express');
+const dir = process.cwd();
+
+const app = express();
+app.use(express.static(dir));
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  let currentDir = dir;
+  let query = req.query.path || '';
+  if (query) currentDir = path.join(dir, query);
+  fs.readdirAsync(currentDir)
+  .then(files => Promise.all(files.map(getStats)))
+  .then(console.log)
+  .catch(err => res.send(err));
 });
 
-server.listen(80);
-console.log('listening on port 80');
+function getStats(file) {
+  return fs.statAsync(path.join(dir, file))
+}
+
+const server = http.createServer(app);
+
+server.listen(80, () => {
+  console.log('listening on port 80');
+});
